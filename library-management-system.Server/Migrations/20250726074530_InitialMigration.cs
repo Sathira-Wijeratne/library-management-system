22c6985c359b11700b5d75379e6072eb14a80aa7
Sathira-Wijeratne <swije27@gmail.com>
2025-07-26 13:36:47 +0530

    Add initial migration and model snapshot for Books table

---
+++ library-management-system.Server/Migrations/20250726074530_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace library_management_system.Server.Migrations
+{
+    // this migration was automatically created using dotnet-ef a command-line tool for Entity Framework Core
+    /// <inheritdoc />
+    /// 
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        /// if this migration is applied, it will attempt to create a new table named Books and will generate columns corresponding to Book entity class. 
+        /// up method - defines what happens when the migration is applied
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(type: "TEXT", nullable: false),
+                    author = table.Column<string>(type: "TEXT", nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        /// down metho - defines how to undo the migration
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
