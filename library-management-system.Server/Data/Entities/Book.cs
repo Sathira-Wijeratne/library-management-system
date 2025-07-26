3f9bb533e352383fc6d79005bb60fd83cab71a6a
Sathira-Wijeratne <swije27@gmail.com>
2025-07-26 13:16:01 +0530

    Add Id property to Book entity for primary key recognition

--- library-management-system.Server/Data/Entities/Book.cs
+++ library-management-system.Server/Data/Entities/Book.cs
@@ -2,6 +2,8 @@
 {
     public class Book
     {
+        // Entity Framework will consider Id as primary key automatically
+        public int Id { get; set; }
         public string title { get; set; }
         public string author { get; set; }
         public string description{ get; set; }
