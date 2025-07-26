eec6d0622e3e6ff7018555348db1ee4ee0bf6c09
Sathira-Wijeratne <swije27@gmail.com>
2025-07-26 12:19:43 +0530

    Add Book entity class with title, author, and description properties

---
+++ library-management-system.Server/Data/Entities/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace library_management_system.Server.Data.Entities
+{
+    public class Book
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public string description{ get; set; }
+    }
+}
