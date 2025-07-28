c719f7e3319c47cd378790e79973beb184fe48fc
Sathira-Wijeratne <swije27@gmail.com>
2025-07-28 19:06:49 +0530

    Remove unncecessary comments in Program.cs

--- library-management-system.Server/Program.cs
+++ library-management-system.Server/Program.cs
@@ -7,8 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 // registers DbContext with the dependency injection system and configures it to use SQLite database file named books.db
-// options are specified instructing to connect to SQLite
-// UseSqlite expects a connection string which is provided
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=books.db"));
 
 builder.Services.AddControllers();
