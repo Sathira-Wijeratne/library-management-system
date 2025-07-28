92736370c56a97b7f39c2468f37bcfdb4229e997
Sathira-Wijeratne <swije27@gmail.com>
2025-07-28 19:06:20 +0530

    Refactor ApplicationDbContext class to add code documentation

--- library-management-system.Server/Data/ApplicationDbContext .cs
+++ library-management-system.Server/Data/ApplicationDbContext .cs
@@ -1,19 +1,23 @@
 ﻿using library_management_system.Server.Data.Entities;
-using Microsoft.EntityFrameworkCore; // gives access to the DbContext class which represents database session, it provides ORM (Object-Relational Mapping) features for database access.
+using Microsoft.EntityFrameworkCore;
 
 namespace library_management_system.Server.Data
 {
-    // defines the application's database context class.
-    public class ApplicationDbContext : DbContext // ApplicationDbContext class which inherits from DbContext. 'DbContext' is a base class from Entity Framework Core. By extending it, you inherit database management features.
+    /// <summary>
+    /// Represents the application's database context, providing access to the database using Entity Framework Core.
+    /// </summary>
+    public class ApplicationDbContext : DbContext
     {
-        // this is a constructor, takes options as a parameter
-        // DbContextOptions specify database configurations such as provider and connection string
-        // : base(options): is syntax for calling the constructor of the base class which ApplicationDbContext is inheriting (DbContext)
-        // so it calls the constructor of the DbContext base class, and pass the options object to it.
+        /// <summary>
+        /// Constructor initializes a new instance of the <see cref="ApplicationDbContext"/> class with the specified options.
+        /// </summary>
+        /// <param name="options">Database provider and connection string</param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        // DbSet represents a table in the database.
-        // so this lets us access and manage all Book records in database.
+        /// <summary>
+        /// Gets or sets the collection of books in the database.
+        /// This property represents the Books table in the database.
+        /// </summary>
         public DbSet<Book> Books { get; set; }
     }
 }
