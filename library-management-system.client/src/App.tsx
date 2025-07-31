8409a1a05c770e6afe9f073f176588bf10794616
Sathira-Wijeratne <swije27@gmail.com>
2025-08-01 02:07:59 +0530

    Add routing for Login and Register pages in App component

--- library-management-system.client/src/App.tsx
+++ library-management-system.client/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Header from './components/Header';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
             <Header/>
                 <Routes>
                     <Route path='/' element={<Home/>}></Route>
+                    <Route path='/login' element={<Login/>}></Route>
+                    <Route path='/register' element={<Register/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
