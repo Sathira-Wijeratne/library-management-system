221f077139b864632ca108ed63d597a36d44cf5c
Sathira-Wijeratne <swije27@gmail.com>
2025-07-31 22:44:41 +0530

    Add JWT authentication configuration in Program.cs

--- library-management-system.Server/Program.cs
+++ library-management-system.Server/Program.cs
@@ -1,6 +1,9 @@
 ﻿using library_management_system.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using library_management_system.Middleware.Exceptions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // registers DbContext with the dependency injection system and configures it to use SQLite database file named books.db
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=books.db"));
+
+// Register JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]!))
+        };
+    });
+builder.Services.AddAuthorization(); 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -32,6 +52,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Apply authentication and authorization middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
